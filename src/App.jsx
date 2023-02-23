5e6cf868b7353e1822d38edc29333e33a42858b6
kumaresan2001 <kumaresancrseven@gmail.com>
2023-02-23 23:07:47 +0530

    day31-task

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import Home from "./components/Home";
+import Header from "./components/Header";
+import NotFound from "./components/NotFound";
+import AddMark from "./components/AddMark";
+import EditMark from "./components/EditMark";
+import ViewMark from "./components/ViewMark";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import Paper from "@mui/material/Paper";
+
+const App = () => {
+  const [mode, setMode] = useState("light");
+  const darkTheme = createTheme({
+    palette: {
+      mode: mode,
+    },
+  });
+  const bgstyles = {
+    borderRadius: "0px",
+    minHeight: "120vh",
+  };
+
+  return (
+    <div>
+      <ThemeProvider theme={darkTheme}>
+        <Paper elevation={3} style={bgstyles}>
+          <Header mode={mode} setMode={setMode} />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/add" element={<AddMark />} />
+            <Route path="/student/edit/:id" element={<EditMark />} />
+            <Route path="/student/view/:id" element={<ViewMark />} />
+            <Route path="/404" element={<NotFound />} />
+            <Route path="*" element={<Navigate replace to="/404" />} />
+          </Routes>
+        </Paper>
+      </ThemeProvider>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/api/api.jsx
@@ -0,0 +1,1 @@
+export const API = "https://63f4ee4d3f99f5855dba9b9b.mockapi.io/users";
